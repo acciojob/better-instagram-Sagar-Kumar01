07611b95e966db242d030b820fa3d196b745ffda
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2025-11-28 16:39:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your code here
+let parent = document.getElementById("parent");
+let images = document.getElementsByClassName("image");
+
+let dragged = null;
+
+// Convert HTMLCollection to Array
+[...images].forEach((value) => {
+
+  value.addEventListener("dragstart", (e) => {
+    dragged = e.target;
+  });
+
+  value.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+
+  value.addEventListener("drop", handleDrop);
+});
+
+function handleDrop(e) {
+  e.preventDefault();
+
+  let target = e.target;
+
+  if (dragged !== target) {
+    let temp = document.createElement("div");
+
+    dragged.before(temp);
+    target.before(dragged);
+    temp.replaceWith(target);
+  }
+}
